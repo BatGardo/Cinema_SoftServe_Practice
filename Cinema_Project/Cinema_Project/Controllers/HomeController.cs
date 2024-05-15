6864fbaa3a30b6e5001419a41a0b68308fc46c91
Hapendo <utgirovec@gmail.com>
2024-05-15 12:15:42 +0300

    MainPageLogic
    DO NOT MERGE
    added data output from DB, almost finished search (rework)

--- Cinema_Project/Cinema_Project/Controllers/HomeController.cs
+++ Cinema_Project/Cinema_Project/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Cinema_Project.Models;
 using System.Diagnostics;
+using Cinema_Project.Migrations;
+using Cinema_Project.Data;
 
 namespace Cinema_Project.Controllers
 {
@@ -9,15 +11,20 @@ namespace Cinema_Project.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
 
         public IActionResult Index()
         {
-            return View();
+            var movies = _context.Movies.ToList();
+            return View(movies);
         }
 
         public IActionResult Privacy()
@@ -30,5 +37,32 @@ namespace Cinema_Project.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        [HttpGet]
+        public IActionResult GetMovieDetails(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(m => m.MovieId == id);
+            return PartialView("_MovieDetailsPartial", movie);
+        }
+
+        [HttpGet]
+        public IActionResult Index(string search)
+        {
+            List<Movie> movies;
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                movies = _context.Movies.Where(m => m.Title.Contains(search)).ToList();
+            }
+            else
+            {
+                movies = _context.Movies.ToList();
+            }
+
+            return View(movies);
+        }
+
+
     }
 }
--- Cinema_Project/Cinema_Project/Migrations/20240515090002_dur_int.Designer.cs
+++ Cinema_Project/Cinema_Project/Migrations/20240515090002_dur_int.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Cinema_Project.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Cinema_Project.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240515090002_dur_int")]
+    partial class dur_int
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
