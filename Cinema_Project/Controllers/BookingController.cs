5b4872ac091b53a95b240a9ae419d3d4633cad68
MULTIGID <multigid@gmail.com>
2024-05-18 14:55:30 +0300

    Ticket booking and crach button "Buy ticket"

---
+++ Cinema_Project/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema_Project.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult BookingView()
+        {
+            return View();
+        }
+    }
+}
