f828dff52ec27b5488e7dd7817459e8cff374548
MULTIGID <multigid@gmail.com>
2024-05-19 02:24:55 +0300

    Trailer fix and many little fixes

--- Cinema_Project/wwwroot/js/trailer-modal.js
+++ Cinema_Project/wwwroot/js/trailer-modal.js
@@ -1,20 +1,30 @@
-var buttons = document.querySelectorAll(".film-trailer-button"); // Виберіть всі елементи з класом "film-trailer-button"
+var buttons = document.querySelectorAll(".activate-modal"); // Виберіть всі елементи з класом "film-trailer-button"
+
+var btn_watch_open = document.getElementById("film-trailer-button");
+var btn_watch_close = document.getElementById("btn_watch_close");
+
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
 var videoPlayer = document.getElementById("videoPlayer");
 videoPlayer.volume = 0.5;
 
 // Додайте обробник подій до кожного елемента з класом "film-trailer-button"
-buttons.forEach(function(button) {
-    button.onclick = function() {
-        modal.style.display = "block";
-        document.body.style.overflow = "hidden"; // блокування прокрутки
-        videoPlayer.play(); // Відтворення відео
+buttons.forEach(function (button) {
+    button.onclick = function () {
+        modal.style.opacity = "1";
+        modal.style.zIndex = "3";
+        document.body.style.overflow = "hidden"; // блокирование прокрутки
+        setTimeout(function () {
+            videoPlayer.play(); // Воспроизведение видео
+        }, 1000); // задержка 3 секунды
     }
 });
 
-span.onclick = function() {
-    modal.style.display = "none";
+span.onclick = function () {
+    modal.style.opacity = "0";
     document.body.style.overflow = ""; // знову дозволяє прокрутку
     videoPlayer.pause(); // Призупинити відтворення відео
+    setTimeout(function () {
+        modal.style.zIndex = "-1"; // Воспроизведение видео
+    }, 1000);
 }
