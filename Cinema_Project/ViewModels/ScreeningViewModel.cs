98748db7b918dbe26d94c1e374c5830671243938
Kurobyro <daynneki@gmail.com>
2024-05-20 01:22:31 +0300

    Admin pannel: Output screening
    Output screening

---
+++ Cinema_Project/ViewModels/ScreeningViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cinema_Project.ViewModels
+{
+    public class ScreeningViewModel
+    {
+        public int Id { get; set; } // Add the Id property
+        public string MovieTitle { get; set; }
+        public string HallName { get; set; }
+        public DateTime ScreeningDate { get; set; }
+    }
+}
