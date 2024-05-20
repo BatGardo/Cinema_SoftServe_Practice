50346bfe61771b2ed20579cdcd77a26aa903db32
ArtemOpria <opria2662@gmail.com>
2024-05-20 12:53:23 +0300

    Display movie and screening
    Added display movie and screening from database

--- Cinema_Project/Controllers/BookingController.cs
+++ Cinema_Project/Controllers/BookingController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cinema_Project.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cinema_Project.Controllers
 {
     public class BookingController : Controller
     {
-        public IActionResult BookingView()
+        private readonly AppDbContext _context;
+
+        public BookingController(AppDbContext context)
         {
+            _context = context;
+        }
+        public IActionResult BookingView(int movieId)
+        {
+            var movie = _context.Movies
+                .Where(m => m.MovieId == movieId)
+                .Select(m => new
+                {
+                    m.Title,
+                    m.Duration,
+                    m.Rating,
+                })
+                .FirstOrDefault();
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            var screenings = _context.Screenings
+                .Where(s => s.MovieId == movieId)
+                .ToList();
+
+            ViewBag.Movie = movie;
+            ViewBag.Screenings = screenings;
+
             return View();
         }
     }
