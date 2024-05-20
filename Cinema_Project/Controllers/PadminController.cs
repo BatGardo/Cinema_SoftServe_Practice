cce5f9034452068bd02c3ad1f315c6f822e7c2ae
Hapendo <utgirovec@gmail.com>
2024-05-20 18:39:26 +0300

    ass-watch
    finished

--- Cinema_Project/Controllers/PadminController.cs
+++ Cinema_Project/Controllers/PadminController.cs
@@ -142,19 +142,6 @@ namespace Cinema_Project.Controllers
             }
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 		public IActionResult AddWatchtime()
 		{
 			var movies = _context.Movies
@@ -177,8 +164,6 @@ namespace Cinema_Project.Controllers
 			return View(model);
 		}
 
-
-
 		[HttpGet]
 		public IActionResult SearchMovies(string query)
 		{
@@ -188,75 +173,6 @@ namespace Cinema_Project.Controllers
 
 			return Json(movies);
 		}
-
-
-		[HttpPost]
-		public async Task<IActionResult> CreateScreening([FromBody] ScreenVM model)
-		{
-			try
-			{
-
-				// Проверка переменных на корректность
-				if (model.Id <= 0)
-				{
-					return BadRequest(new { message = "Invalid movieId" });
-				}
-
-				if (model.HallName <= 0)
-				{
-					return BadRequest(new { message = "Invalid hallNumber" });
-				}
-
-				if (string.IsNullOrWhiteSpace(model.Date))
-				{
-					return BadRequest(new { message = "Date is required" });
-				}
-
-				if (!DateTime.TryParse(model.Date, out DateTime parsedDate))
-				{
-					return BadRequest(new { message = "Invalid date format" });
-				}
-
-				if (model.Time < 0 || model.Time > 23)
-				{
-					return BadRequest(new { message = "Invalid time" });
-				}
-
-				if (model.Price < 0)
-				{
-					return BadRequest(new { message = "Invalid price" });
-				}
-
-				// Преобразуем строку с датой и время в DateTime
-				DateTime screeningDate = parsedDate.Date + TimeSpan.FromHours(model.Time);
-
-				// Создаем новый объект Screening
-				//var screening = new Screening
-				//{
-				//	MovieId = movieId,
-				//	HallNumber = hallNumber,
-				//	ScreeningDate = screeningDate,
-				//	Price = price
-				//};
-
-				// Добавляем новый объект в контекст данных
-				//_context.Screenings.Add(screening);
-
-				// Сохраняем изменения в базе данных
-				await _context.SaveChangesAsync();
-
-				// Возвращаем результат успешного выполнения
-				return Ok(new { message = "Screening created successfully" });
-			}
-			catch (Exception ex)
-			{
-				// Если произошла ошибка, возвращаем статус 500 с сообщением об ошибке
-				return StatusCode(500, new { message = "Error creating screening", error = ex.Message });
-			}
-		}
-
-
-
 
 
 	}
